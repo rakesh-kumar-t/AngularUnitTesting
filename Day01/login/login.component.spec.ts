ac6265703ed01a63813c9cb3990035b0c711829c
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2022-01-27 16:01:00 +0530

    Update login.component.spec.ts

--- Day01/login/login.component.spec.ts
+++ Day01/login/login.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms';
 
 describe('LoginComponent', () => {
+
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [ FormsModule ]
     })
     .compileComponents();
   });
@@ -56,5 +58,94 @@ describe('LoginComponent', () => {
 });
 
 
+//6. Testing component templates --- access html tags
+it('should generate "User Login" in legend tag', () => {
+  const tagObj  =  fixture.nativeElement.querySelector("legend");
+  // console.log(tagObj.textContent);
+  let str  = tagObj.textContent;
+  expect(str).toBe("User Login");
+});
+
+
+//7. Testing component templates --- access html tags bind with variables
+it('should generate empty in result paragraph', () => {
+  const tagObj  =  fixture.nativeElement.querySelector("p");
+  let str  = tagObj.textContent;
+  expect(str).toBe("");
+});
+
+//8. Testing component templates --- input fields
+it('should set the value User Id textbox', () => {
+  const inputObj  =  fixture.nativeElement.querySelector("input");
+  inputObj.value = "Scott"; // set
+  let str = inputObj.value; //get
+  expect(str).toBe("Scott");
+});
+
+
+//9. Testing component templates --- 2-way data binding
+it('should set the value to component variable using textbox', () => {
+  const inputObj  =  fixture.nativeElement.querySelector("input");
+  inputObj.value = "Scott"; // set
+  inputObj.dispatchEvent( new Event("input") ); // reflect two-way data binding
+  let str = component.uid;
+  expect(str).toBe("Scott");
+});
+
+
+//10. Testing component templates --- button click event
+it('should generate invalid message when user click the button without textbox values', () => {
+  const inputArray =  fixture.nativeElement.querySelectorAll("input");
+
+  inputArray[2].dispatchEvent( new Event("click") );
+
+  // console.log(component.result);
+
+  fixture.detectChanges();
+  const tagObj  =  fixture.nativeElement.querySelector("p");
+  let str  = tagObj.textContent;
+  expect(str).toBe("Invalid user id or password");
+});
+
+
+//11. Testing component templates --- button click event
+it('should generate Welcome message for valid credentials when user click login button', () => {
+  const inputArray =  fixture.nativeElement.querySelectorAll("input");
+
+  inputArray[0].value = "admin"; // set
+  inputArray[0].dispatchEvent( new Event("input") ); // reflect two-way data binding
+
+  inputArray[1].value = "admin123"; // set
+  inputArray[1].dispatchEvent( new Event("input") ); // reflect two-way data binding
+
+  inputArray[2].dispatchEvent( new Event("click") );
+  fixture.detectChanges();
+
+
+
+  const tagObj  =  fixture.nativeElement.querySelector("p");
+  let str  = tagObj.textContent;
+  expect(str).toBe("Welcome to Admin");
+});
+
+
+//12. Testing component templates --- button click event
+it('should generate Invalid message for invalid credentials when user click login button', () => {
+  const inputArray =  fixture.nativeElement.querySelectorAll("input");
+
+  inputArray[0].value = "scott"; // set
+  inputArray[0].dispatchEvent( new Event("input") ); // reflect two-way data binding
+
+  inputArray[1].value = "hello123"; // set
+  inputArray[1].dispatchEvent( new Event("input") ); // reflect two-way data binding
+
+  inputArray[2].dispatchEvent( new Event("click") );
+  fixture.detectChanges();
+
+
+  const tagObj  =  fixture.nativeElement.querySelector("p");
+  let str  = tagObj.textContent;
+  expect(str).toBe("Invalid user id or password");
+});
 
 });
